3e94b586ace33755c7de213958a3db27036c08c4
Icarus-Community <icarus.alvey@gmail.com>
2024-06-28 21:40:36 +0200

    v

--- styles/theme.js
+++
@@ -1,9 +0,0 @@
-export const theme = {
-  colors: {
-    primary: '#FFA500', // Golden Orange
-    background: '#121212',
-    cardBackground: '#1E1E1E',
-    text: '#FFFFFF',
-  },
-  fonts: ['Roboto', 'sans-serif'],
-};
