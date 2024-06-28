86ac3ceda12dc5ca46b08f3593d06bb62dcc25ef
Icarus-Community <icarus.alvey@gmail.com>
2024-06-28 21:40:24 +0200

    bv

---
+++ pages/pages/_app.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { ThemeProvider } from 'styled-components';
+import GlobalStyle from '../styles/GlobalStyles';
+import { theme } from '../styles/theme';
+
+const MyApp = ({ Component, pageProps }) => {
+    return (
+        <ThemeProvider theme={theme}>
+            <GlobalStyle />
+            <Component {...pageProps} />
+        </ThemeProvider>
+    );
+};
+
+export default MyApp;
